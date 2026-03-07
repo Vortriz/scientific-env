ff28e2eda55c5679d399779cdcb792fd566cdc84
Vortriz <vorarishi22+github@gmail.com>
2026-03-07 16:25:55 +0530

    tweak(python): enable marimo in nix fmt

--- templates/default/nix/python/fmt.nix
+++ templates/default/nix/python/fmt.nix
@@ -1,36 +1,35 @@
 {
-    perSystem = {
-        treefmt = {
-            programs = {
-                ruff-format.enable = true;
-                ruff-check = {
-                    enable = true;
-                    extendSelect = [ "I" ];
+    perSystem =
+        { pkgs, ... }:
+        {
+            treefmt = {
+                programs = {
+                    ruff-format.enable = true;
+                    ruff-check = {
+                        enable = true;
+                        extendSelect = [ "I" ];
+                    };
                 };
-            };
 
-            settings.formatter = {
-                ruff-format.priority = 1;
-                ruff-check = {
-                    priority = 2;
-                    options = [ "--fix-only" ];
+                settings.formatter = {
+                    ruff-format.priority = 1;
+                    ruff-check = {
+                        priority = 2;
+                        options = [ "--fix-only" ];
+                    };
+                    marimo = {
+                        priority = 3;
+                        command = pkgs.lib.getExe pkgs.python3Packages.marimo;
+                        options = [
+                            "check"
+                            "."
+                            "--fix"
+                            "--unsafe-fixes"
+                            "--ignore-scripts"
+                        ];
+                        includes = [ "*.py" ];
+                    };
                 };
-                # [MARK] wait for https://github.com/NixOS/nixpkgs/pull/443053 to go through
-                # marimo = {
-                #     priority = 3;
-                #     command = pkgs.lib.getExe pkgs.uv;
-                #     options = [
-                #         "run"
-                #         "marimo"
-                #         "check"
-                #         "."
-                #         "--fix"
-                #         "--unsafe-fixes"
-                #         "--ignore-scripts"
-                #     ];
-                #     includes = [ "*.py" ];
-                # };
             };
         };
-    };
 }
